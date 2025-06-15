d33166097a12f9aec88b890b79e614c2f27ad850
DESKTOP-PCO4BR9\student <test>
2025-06-15 10:22:15 +0700

    MakeinThailand

---
+++ makeinthailand.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, fname,lname, age, courses ,pocket ,location):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.courses = courses
+        self.pocket = pocket
+        self.location = location
+
+    def display_info(self):
+        print("Name:", self.fname,self.lname)
+        
+        print("Age:", self.age)
+        print("Courses:", ", ".join(self.courses))
+        print('pocket :',self.pocket)
+        print("location :",self.location)
+        print("-" * 20)
+
+student1 = Student("Alice",'Inwonderland', 20, ["Math", "English"],100,'540 M6')
+student2 = Student("Bob",'Biechawton', 22, ["Physics", "History"],500 , '654 M5')
+
+student1.display_info()
+student2.display_info()
