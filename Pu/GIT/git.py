a6a73e872f6930ecc74a898d0af422ef2026920e
DESKTOP-PCO4BR9\student <test>
2025-06-15 15:44:57 +0700

    Heal Added

--- Pu/GIT/git.py
+++ Pu/GIT/git.py
@@ -8,11 +8,17 @@ class NPC:
         print(f"Name {self.name}",end=" ")
         print(f"HP {self.hp}",end=" ")
         print(f"Mana {self.mana}",end=" \n")
+    
+    def heal(self,heal):
+        self.hp += heal
 
 n1 = NPC("A",100,100)
 n2 = NPC("B",500,200)
 n3 = NPC("C",1000,1000)
 
 n1.describe()
+n2.describe()
+n2.heal(100)
+n2.heal(30)
 n2.describe()
 n3.describe()
