18f76f313d4fb05448154a41929ba8c965292ce5
DESKTOP-PCO4BR9\student <test>
2025-06-15 11:21:20 +0700

    Add money

--- makeinthailand.py
+++ makeinthailand.py
@@ -1,26 +1,26 @@
 class Student:
-    def __init__(self, fname,lname, age, courses ,pocket ,location):
+    def __init__(self, fname,lname, age, courses ,pocket ,sarary):
         self.fname = fname
         self.lname = lname
         self.age = age
         self.courses = courses
         self.pocket = pocket
-        self.location = location
+        self.sarary = sarary
 
     def display_info(self):
         print("Name:", self.get_fullname())    
         print("Age:", self.age)
         print("Courses:", ", ".join(self.courses))
         print('pocket :',self.pocket)
-        print("location :",self.location)
+        print("sarary :",self.sarary)
         print("-" * 20)
 
     def get_fullname(self):
 
         return f"{self.fname} {self.lname}"
 
-student1 = Student("Alice",'Inwonderland', 20, ["Math", "English"],100,'540 M6')
-student2 = Student("Bob",'Biechawton', 22, ["Physics", "History"],500 , '654 M5')
+student1 = Student("Alice",'Inwonderland', 20, ["Math", "English"],100,'540M')
+student2 = Student("Bob",'Biechawton', 22, ["Physics", "History"],500 , '654M')
 
 student1.display_info()
 student2.display_info()
