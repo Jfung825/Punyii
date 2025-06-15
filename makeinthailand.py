7c98d392b301d6e8557b709aa17b881243ce3e41
DESKTOP-PCO4BR9\student <test>
2025-06-15 10:59:49 +0700

    Get fullname

--- makeinthailand.py
+++ makeinthailand.py
@@ -8,13 +8,16 @@ class Student:
         self.location = location
 
     def display_info(self):
-        print("Name:", self.fname,self.lname)
-        
+        print("Name:", self.get_fullname())    
         print("Age:", self.age)
         print("Courses:", ", ".join(self.courses))
         print('pocket :',self.pocket)
         print("location :",self.location)
         print("-" * 20)
+
+    def get_fullname(self):
+
+        return f"{self.fname} {self.lname}"
 
 student1 = Student("Alice",'Inwonderland', 20, ["Math", "English"],100,'540 M6')
 student2 = Student("Bob",'Biechawton', 22, ["Physics", "History"],500 , '654 M5')
